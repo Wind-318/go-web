81c7fb6df320f03e6dba525cfa0e72c0e3b4f0cf
Wind-318 <68177960+Wind-318@users.noreply.github.com>
2021-03-20 17:50:29 +0800

    Go并发更新

--- GO/并发.go
+++ GO/并发.go
@@ -4,6 +4,7 @@ package main
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 //声明一个等待组
@@ -33,6 +34,42 @@ func main() {
 		//使用go关键字开启一个协程，主线程继续向后执行
 		go hello(ch)
 	}
-	//等待所有任务执行完毕
+
+	//声明一个互斥锁
+	mutex := &sync.Mutex{}
+	wg.Add(2)
+	go mutexTest(mutex)
+	go mutexTest(mutex)
+
+	//声明一个读写锁
+	rwMutex := &sync.RWMutex{}
+
+	//等待所有任务执行完毕，即等待组计数为0
 	wg.Wait()
+}
+
+func mutexTest(mutex *sync.Mutex) {
+	defer wg.Done()
+	//上互斥锁，此时其它线程进入会尝试加锁，如果已经是加锁状态，会堵塞在这里
+	mutex.Lock()
+	//互斥锁解锁
+	defer mutex.Unlock()
+	fmt.Println("测试互斥锁")
+	time.Sleep(time.Second)
+}
+
+func RWmutexTest1(rwMutex *sync.RWMutex) {
+	//上读锁，可以有多个线程进入加多个锁
+	rwMutex.RLock()
+	defer rwMutex.RUnlock()
+
+	//work...
+}
+
+func RWmutexTest2(rwMutex *sync.RWMutex) {
+	//上写锁，只能由一个线程进入加锁，其它线程阻塞
+	rwMutex.Lock()
+	defer rwMutex.Unlock()
+
+	//work...
 }
